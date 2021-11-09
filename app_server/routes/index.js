9cc02e40e6d158541126ffc6f00c22791df2fcf7
STUDENT Blayne Ahern <Blayne.Ahern@students.ittralee.ie>
2021-11-09 12:28:09 +0000

    added car search function

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const ctrlMain = require('../controllers/main'); 
 /* GET home page. */
-router.get('/', ctrlMain.index); 
+router.get('/index', ctrlMain.index); 
 router.get('/viewcars', ctrlMain.viewcars);
 router.get('/signup', ctrlMain.signup);
 module.exports = router;
